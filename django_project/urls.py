652e065011b24fa51c7d3d13cefd3a77001c611b
Dev3rt0n <everton.osle@gmail.com>
2019-02-20 19:08:21 -0300

    Adição da historia do django no template e mudanças no bootstrap

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', include('home.urls')),
     path('admin/', admin.site.urls),
-    path('home/', views.Homepage),
+    path('', views.Homepage, name='home'),
 ]
